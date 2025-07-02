9ba33d234a44614f5dd7483412e1ff85a0e4be22
Michelle <michelle@thewcl.com>
2025-07-01 19:16:07 -0500

    added stretch challenge

--- hello_world.py
+++ hello_world.py
@@ -2,3 +2,7 @@ print("Welcome to AI Applications!")
 print("What is your name?")
 name = input()
 print("Hello, " + name + "! It’s great to meet you")
+
+print("What's your favorite programming language?")
+language = input()
+print("So cool! " + language + " is great")
